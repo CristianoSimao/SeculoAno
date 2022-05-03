ad7bdef76beec4f2dfbf6255eefa217a2467a86f
Cristiano Simao <40866219+CristianoSimao@users.noreply.github.com>
2022-05-03 09:42:51 -0300

    Add files via upload

---
+++ SeculoAno.php
@@ -0,0 +1,14 @@
+<?php
+    //Resposta da questão 1 "Função SeculoAno#
+    //Código criado por: Cristiano Simão dos Santos
+    
+    function SeculoAno(int $ano){
+        $seculo = $ano / 100;
+        if ($ano % 100){
+            $seculo++;
+        }
+        return intval($seculo);
+    }
+    
+    //echo "resultado: ". SeculoAno(1700); //Teste de execução
+?>
