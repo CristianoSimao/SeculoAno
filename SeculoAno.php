7796bf04784686b3562f3e56917758814ec98e4e
Cristiano Simao <40866219+CristianoSimao@users.noreply.github.com>
2022-05-03 09:43:47 -0300

    Update SeculoAno.php

--- SeculoAno.php
+++ SeculoAno.php
@@ -1,5 +1,5 @@
 <?php
-    //Resposta da questão 1 "Função SeculoAno#
+    //Resposta da questão 1 "Função SeculoAno"
     //Código criado por: Cristiano Simão dos Santos
     
     function SeculoAno(int $ano){
